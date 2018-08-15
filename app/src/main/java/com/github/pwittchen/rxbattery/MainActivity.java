3ed223928e19dfcfd343786398737f9ab847c976
Piotr Wittchen <piotr@wittchen.io>
2018-08-15 18:43:48 +0200

    adding sample app in Kotlin

--- app/src/main/java/com/github/pwittchen/rxbattery/MainActivity.java
+++ app/src/main/java/com/github/pwittchen/rxbattery/MainActivity.java
@@ -1,10 +1,10 @@
 package com.github.pwittchen.rxbattery;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import com.github.pwittchen.reactivebattery.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
