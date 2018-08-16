d92cdb4efb6602e8709440ad247a52e403ef9ed1
Piotr Wittchen <piotr@wittchen.io>
2018-08-16 14:13:29 +0200

    adding BatteryPlugged and BatteryStatus enums

---
+++ library/src/main/kotlin/com/github/pwittchen/rxbattery/library/BatteryPlugged.kt
@@ -0,0 +1,7 @@
+package com.github.pwittchen.rxbattery.library
+
+enum class BatteryPlugged {
+  AC,
+  USB,
+  WIRELESS,
+}
---
+++ library/src/main/kotlin/com/github/pwittchen/rxbattery/library/BatteryStatus.kt
@@ -0,0 +1,9 @@
+package com.github.pwittchen.rxbattery.library
+
+enum class BatteryStatus {
+  UNKNOWN,
+  CHARGING,
+  DISCHARGING,
+  NOT_CHARGING,
+  FULL
+}
